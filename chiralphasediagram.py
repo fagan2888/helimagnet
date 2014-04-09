46d0a52e4a43b40b28d2ce4d57659163a3239f29
Kwan-Yuet Ho, Ph.D. <stephenhky@yahoo.com.hk>
2014-04-09 17:36:55 -0400

    Update chiralphasediagram.py

--- chiralphasediagram.py
+++ chiralphasediagram.py
@@ -10,9 +10,20 @@ import conicalphase as cp
 import helicalphase as hp
 
 class PhaseDeterminationMachine:
-    def __init__(self, configdict, phaseclasses):
-        self.configdict = configdict
+    def __init__(self, config, phaseclasses):
+        self.config = config
         self.phaseclasses = phaseclasses
+        self.points = []
+
+    def compute_phase_energies(self, ind_param):
+        phase_energies = {}
+        for phaseclass in self.phaseclasses:
+            phaseclass.recompute_config(self.config, ind_param)
+            phase_energies[phaseclass.phase_name()] = phaseclass.fden
+        return phase_energies
         
-    
+    def determine_phase(self, ind_param):
+        phase_energies = self.compute_phase_energies(ind_param)
+        sorted_phase_energies = sorted(phase_energies.items(), key=lambda item: item[1], reverse=True)
+        return sorted_phase_energies[0][0]
     
